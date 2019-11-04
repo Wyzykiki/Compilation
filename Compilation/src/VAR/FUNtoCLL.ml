eb0ed877edee0c50c1c813ecc994a0b010409418
Kyllian <47327666+Wyzykiki@users.noreply.github.com>
2019-11-05 00:16:18 +0100

    Begining of FUN

--- Compilation/src/VAR/FUNtoCLL.ml
+++ Compilation/src/VAR/FUNtoCLL.ml
@@ -1,3 +1,6 @@
+module F = FUNInstr
+module C = CLLInstr
+
 (* Dans les expressions, remplace les paramètres formels d'une fonction par 
    le calcul d'adresse correspondant. *)
 let rec translate_expression expr param_table =
@@ -7,13 +10,24 @@ let rec translate_expression expr param_table =
 (* Instructions ne faisant pas référence aux fonctions : traduction iso *)
 (* Pour Call et Return, appliquer le protocole *)
 let rec translate_instruction instr param_table =
-  failwith "not implemented"
+  match instr with
+    | F.Nop -> [ C.Nop ]
+    | F.Exit -> [ C.Exit ]
+    | F.Print(e) -> [ C.Print(e) ]
+    | F.Write(le, e) -> [ C.Write(le, e) ]
+    | F.If(c, s1 ,s2) -> [ C.If(c, translate_sequence s1 param_table, translate_sequence s2 param_table) ]
+    | F.While(c, s) -> [ C.While(c, translate_sequence s param_table) ]
+    | F.Call(d, f, args) -> failwith "not implemented" (* C.Write(d, Name("function_return")) *)
+    | F.Return(e) ->  failwith "not implemented"
 
-    
+and translate_sequence s param_table =
+  List.flatten (List.map translate_instruction s param_table)
+
 let translate_function_definition fdef =
-  failwith "not implemented"
+  { CLL.name = FUN.(fdef.name);
+    CLL.code = translate_sequence FUN.(fdef.code) }
 
     
 let translate_program prog =
-  { CLL.text = List.map translate_function_definition FUN.(prog.text);
-    CLL.data = failwith "not implemented" }
+  { CLL.text = List.flatten (List.map translate_function_definition FUN.(prog.text));
+    CLL.data = FUN.(prog.data) }
